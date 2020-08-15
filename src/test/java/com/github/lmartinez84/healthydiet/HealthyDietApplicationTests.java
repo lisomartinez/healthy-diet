23ed02dbd0f08a53294750884b95bfb08ac0591d
Lisandro Martinez <lisandromartinez@gmail.com>
2020-08-15 17:17:44 +0000

    Initial Commit

---
+++ src/test/java/com/github/lmartinez84/healthydiet/HealthyDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lmartinez84.healthydiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyDietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
